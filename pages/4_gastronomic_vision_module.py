3eaf177522d5fa82f7bf130bb3ddb2d2a801d0b2
Joao D Martire <70855429+jhmartire@users.noreply.github.com>
2023-04-25 19:38:42 +0100

    Update 4_gastronomic_vision_module.py

--- pages/4_gastronomic_vision_module.py
+++ pages/4_gastronomic_vision_module.py
@@ -7,7 +7,6 @@ import pandas               as pd
 import numpy                as np
 import plotly.express       as px
 import plotly.graph_objects as go
-import seaborn              as sns
 import streamlit            as st
 import inflection
 import folium
